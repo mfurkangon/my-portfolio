8b4084de593f882dfb1ac91c4bc50c2a25d7fadb
mafugo <mahmutfurkangon16400@gmail.com>
2024-01-23 21:59:57 +0300

    site bok gibi oldu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
+      <div className="flex justify-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0 text-xl">
           <a href="#home" className="ml-3 text-xl">
             Home
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <nav className="flex flex-row items-center text-base">
           <a href="#research" className="mr-5 hover:text-white">
             Research
           </a>
