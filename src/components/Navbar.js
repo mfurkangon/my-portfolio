4607ec0d8429a829f90f0c8d4cf507f02330eec9
mafugo <mahmutfurkangon16400@gmail.com>
2024-01-23 21:51:21 +0300

    started modifying the website

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
@@ -6,14 +5,16 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Mahmut Furkan Gön
+          <a href="#home" className="ml-3 text-xl">
+            Home
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+          <a href="#research" className="mr-5 hover:text-white">
+            Research
           </a>
+        </nav>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
@@ -21,8 +22,7 @@ export default function Navbar() {
         <a
           href="#contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Contact Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
+          Contact
         </a>
       </div>
     </header>
